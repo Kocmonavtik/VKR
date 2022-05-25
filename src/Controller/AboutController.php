c8cf91c5b4f336870ec636963e73f5d702666586
Kocmonavtik <chapligin141299@gmail.com>
2022-05-25 07:39:47 -0700

    Создание котроллера

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutController extends AbstractController
+{
+    /**
+     * @Route("/about", name="app_about")
+     */
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
+        ]);
+    }
+}
