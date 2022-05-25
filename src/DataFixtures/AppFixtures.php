15b65634cd21234cf581499c6181c3d4426d1ede
Kocmonavtik <chapligin141299@gmail.com>
2022-05-25 07:01:27 -0700

    хэширование паролей и добавление токена CSRF

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,16 +5,24 @@ namespace App\DataFixtures;
 use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
-        $user= new Users();
+        $user = new Users();
         $user->setEmail('commonUser@gmail.com');
-        $user->setPassword('12345');
+        $user->setPassword($this->passwordHasher->hashPassword($user,'12345'));
         $user->setRoles(['ROLE_USER']);
         $user->setAvatar('');
         $user->setGender('men');
